dffbdcefe9076fc64ba3bfd95578acdc57dd75cd
Julian Rios <julianrios.dev@gmail.com>
2018-05-26 12:22:40 -0400

    created two soulutions to reverse a String

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,26 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        String result = new StringBuffer(inputString).reverse().toString();
+        return result;
+		
+
+		// Second approach remember to create a String for the char[] result returned 
+		
+		// Learned I can created a string from an array of characters!
+
+		/* String reverse = "";
+
+    	char [] in = inputString.toCharArray();
+    	char [] result = new char [in.length];
+    	int len = in.length;
+    	for (char c : in) {
+    		result[len-1] = c; // inserts at the end of the array of result
+    		len--;
+    	}
+    	reverse = (new String(result));
+    	return reverse;
+    	**/
+    } 
   
 }
